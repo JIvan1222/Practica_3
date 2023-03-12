016d2aac599e50254747078d2ecf9d16d398835d
JIvan1222 <127069493+JIvan1222@users.noreply.github.com>
2023-03-12 06:47:05 -0600

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,10 +1,5 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Socket;
+
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 public class Cliente {
